0e0c889a7c70494f1985b41bd42e10d8e5dcf217
Jacob Hein <jacobhein1892@gmail.com>
2023-04-19 17:42:44 -0700

    added and populated the test.txt file which is going to be used until we are ready to move to user Input

---
+++ main.c
@@ -0,0 +1,15 @@
+// Implement a basic command-line application that reads data from a text file and performs operations such as word count, line count, and character count.
+
+#include <stdio.h>
+
+int main(){
+    // Ask user for Text file
+        // Start with just having a text file in this folder
+    // Go into the text file
+
+    // Count the words
+    
+    // Count the lines
+
+    // Count the characters (excluding spaces and punctuation)
+}
